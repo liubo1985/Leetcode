17b3e7f66bccc1ea500aef41ada98c43a3b1e261
liubo31 <liubo31@hotmail.com>
2016-06-28 00:24:40 -0700

    MaximumDepthofBinaryTree

--- CC150/MaximumDepthofBinaryTree/Solution.java
+++ CC150/MaximumDepthofBinaryTree/Solution.java
@@ -3,5 +3,30 @@ package CC150.MaximumDepthofBinaryTree;
 /**
  * Created by bliu on 5/15/16.
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 }
