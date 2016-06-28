6dd93c871d4c8f1d4cab812cb74f5f3366b06bf0
liubo31 <liubo31@hotmail.com>
2016-06-28 00:25:29 -0700

    RemoveDuplicatesFromSortedList

--- CC150/RemoveDuplicatesFromSortedList/Solution.java
+++ CC150/RemoveDuplicatesFromSortedList/Solution.java
@@ -3,5 +3,39 @@ package CC150.RemoveDuplicatesFromSortedList;
 /**
  * Created by bliu on 5/12/16.
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+        head = dummy;
+
+        ListNode next = head.next;
+        ListNode nonDup = head;
+        while(next != null){
+            if(next.val == nonDup.val){
+                next = next.next;
+            }
+            else{
+                nonDup.next = next;
+                nonDup = next;
+            }
+        }
+        //Do not forget put null to the tail
+        nonDup.next = null;
+        return dummy.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 }
