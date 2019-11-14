385ee337934d74860179a87d87db3601c2125d3d
Bo Liu <bol@zoosk.com>
2019-11-14 11:45:41 -0800

    Add more problems

--- CC150/RemoveDuplicatesfromSortedList2/Solution.java
+++ CC150/RemoveDuplicatesfromSortedList2/Solution.java
@@ -1,7 +1,7 @@
-package CC150.RemoveDuplicatesfromSortedList2;
+package CC150.RemoveDuplicatesFromSortedList2;
 
 /**
- * Created by bliu on 4/27/2016.
+ * Created by bliu on 5/12/16.
  */
 
 /**
@@ -17,44 +17,30 @@ public class Solution {
         if(head == null){
             return null;
         }
-        ListNode dummy = new ListNode(0);
+
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
         dummy.next = head;
         head = dummy;
-        ListNode pre = head;
-        ListNode cur = head.next;
 
-        boolean currentDup = false;
-        while(cur != null){
-            //test if next node is same as current node, if true, remove next node, mark dup flag true
-            if(cur.next != null && cur.next.val == cur.val){
-                ListNode next = cur.next;
-                cur.next = next.next;
-                currentDup = true;
-            }
-            //if next node is not equal to current node, check if dup flag is true, if yes, remove current node
-            else if(currentDup == true){
-                pre.next = cur.next;
-                cur = cur.next;
+        //compare the next two nodes, if equal, using while statement remove them
+        while(head.next != null && head.next.next != null){
+            if(head.next.val == head.next.next.val){
+                int val = head.next.val;
+                while(head.next != null && head.next.val == val){
+                    head.next = head.next.next;
+                }
 
-                currentDup = false;
             }
-            //if next node is not equal to current node and dup flag is also flase, which means current node is unique
             else{
-                pre = cur;
-                cur = cur.next;
-
+                head = head.next;
             }
-
         }
-        return head.next;
+        return dummy.next;
     }
 
     public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
+           int val;
+           ListNode next;
+           ListNode(int x) { val = x; }
     }
 }
